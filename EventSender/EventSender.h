027fc08b35c7229d6595297bbbcffcc2cf05a02d
Alexey Ivankov <mail.alexey.general@gmail.com>
2017-03-18 00:46:27 +0300

    initial

---
+++ EventSender/EventSender.h
@@ -0,0 +1,19 @@
+//
+//  EventSender.h
+//  EventSender
+//
+//  Created by Alexey Ivankov on 18.03.17.
+//  Copyright © 2017 Alexey Ivankov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EventSender.
+FOUNDATION_EXPORT double EventSenderVersionNumber;
+
+//! Project version string for EventSender.
+FOUNDATION_EXPORT const unsigned char EventSenderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EventSender/PublicHeader.h>
+
+
